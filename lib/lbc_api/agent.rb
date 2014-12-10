523e08bf2b3f4fb1b55975111427c152899965cf
Thibault Couraud <thibault@gomesbrothers.com>
2014-12-10 17:42:13 +0100

    No keep alive / Fix Delete / Sleep

--- lib/lbc_api/agent.rb
+++ lib/lbc_api/agent.rb
@@ -4,6 +4,7 @@ module LbcApi
       Mechanize.new do |agent|
         agent.user_agent_alias = 'Mac Safari'
         agent.follow_meta_refresh = true
+        agent.keep_alive = false
       end
     end
   end
--- lib/lbc_api/classified.rb
+++ lib/lbc_api/classified.rb
@@ -2,7 +2,9 @@ module LbcApi
   class Classified
     class << self
       def create(email, password, data, files = nil)
-        page = Agent.mecha.get('http://www2.leboncoin.fr/ai')
+        agent = Agent.mecha
+        page = agent.get('http://www2.leboncoin.fr/ai')
+        sleep 3
         data = Sanitize.params(data).merge({'email' => email})
         if files && files.count
           files.each_with_index do |img, i|
@@ -10,7 +12,8 @@ module LbcApi
               f.file_uploads[i].file_name = img
               f['extra_images'] = 1
             end.submit
-            Agent.mecha.get(page.search('.photo.waiting').at('span')['title'])
+            agent.get(page.search('.photo.waiting').at('span')['title'])
+            sleep 3
           end
         end
         page = page.form_with(name: 'formular') do |f|
@@ -18,18 +21,22 @@ module LbcApi
             f[k] = v
           end
         end.submit
+        sleep 3
         page.form_with(name: 'formular') do |f|
           f['passwd'] = password
           f['passwd_ver'] = password
         end.submit
+        sleep 3
+        agent.shutdown
       end
 
       def delete(id, password)
         page = Agent.mecha.get("http://www2.leboncoin.fr/ai?id=#{id}&cmd=delete")
         page = page.forms[page.forms.count - 2].submit
-        page.forms.last do |f|
-          f['password'] = password
+        page = page.form_with(action: '/ai/delete/0') do |f|
+          f['passwd'] = password
         end.submit
+        !!page.at('h1:contains("Votre annonce sera supprimée")')
       end
 
       def exists?(title)
